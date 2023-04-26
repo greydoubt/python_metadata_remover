111db3ee2c32525f16f986579119faf8110ca162
Sean M <43443470+greydoubt@users.noreply.github.com>
2023-04-25 23:21:06 -0700

    functional programming variant
    added decorator function for creating output folder

--- python_metadata_cleaner.py
+++ python_metadata_cleaner.py
@@ -1,9 +1,19 @@
-"""Python script that can remove all EXIF metadata from a folder of image files or a single image file and output the cleaned files to a new folder with the same name as the original folder or file, with the suffix "_cleaned" added. The cleaned files will have the same names as the original files"""
-
 import os
 import sys
 from PIL import Image
 
+def create_output_folder(func):
+    def wrapper(*args, **kwargs):
+        output_folder = kwargs.get("output_folder", None)
+        if output_folder is None:
+            input_folder = args[0]
+            output_folder = input_folder + "_cleaned"
+            os.makedirs(output_folder, exist_ok=True)
+            kwargs["output_folder"] = output_folder
+        return func(*args, **kwargs)
+    return wrapper
+
+@create_output_folder
 def clean_image_metadata(image_path, output_folder):
     with Image.open(image_path) as img:
         # Remove EXIF data
@@ -20,17 +30,14 @@ if __name__ == '__main__':
     if len(sys.argv) == 2:
         # Clean a single image file
         image_path = sys.argv[1]
-        output_folder = os.path.dirname(image_path) + "_cleaned"
-        os.makedirs(output_folder, exist_ok=True)
-        clean_image_metadata(image_path, output_folder)
+        clean_image_metadata(image_path)
     elif len(sys.argv) == 3:
         # Clean all image files in a folder
         input_folder = sys.argv[1]
-        output_folder = input_folder + "_cleaned"
-        os.makedirs(output_folder, exist_ok=True)
+        output_folder = sys.argv[2]
         for filename in os.listdir(input_folder):
             if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
                 image_path = os.path.join(input_folder, filename)
-                clean_image_metadata(image_path, output_folder)
+                clean_image_metadata(image_path, output_folder=output_folder)
     else:
         print("Usage: python clean_image_metadata.py <input_folder or image_file> [output_folder]")
